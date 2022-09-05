b900d263b50fc31775c0e6627e87a054bcb6e203
Ashwin444 <109952563+Ashwin444@users.noreply.github.com>
2022-09-05 15:41:41 +0530

    commit 1

--- Main.py
+++ Main.py
@@ -10,15 +10,43 @@ class Stack:
 
   def push(self, data) -> None:
     # Write your code here
-
+      if self.head is None:
+            self.head = Node(data)
+      else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
   def pop(self) -> None:
     # Write your code here
+     if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
 
   def status(self):
     """
     It prints all the elements of stack.
     """
     # Write your code here  
+    while True:
+    print('push <value>')
+    print('pop')
+    print('quit')
+    do = input('What would you like to do? ').split()
+ 
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        a_stack.push(int(do[1]))
+    elif operation == 'pop':
+        popped = a_stack.pop()
+        if popped is None:
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', int(popped))
+    elif operation == 'quit':
+        
 
 
 # Do not change the following code
