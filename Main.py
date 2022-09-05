07de1978adffa72f7ba761ff5e8b6b22068c3cab
Ashwin444 <109952563+Ashwin444@users.noreply.github.com>
2022-09-05 15:49:40 +0530

    commit 4

--- Main.py
+++ Main.py
@@ -10,15 +10,21 @@ class Stack:
 
   def push(self, data) -> None:
     # Write your code here
-    new_node = Node(data)
-    new_node.next = self.head
-    self.head = new_node
+    if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
 
   def pop(self) -> None:
     # Write your code here
-    temp = self.head
-    self.head = temp.next
-    temp.next = None
+       if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
 
   def status(self):
     """
